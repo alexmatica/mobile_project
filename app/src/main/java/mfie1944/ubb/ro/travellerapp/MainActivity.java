cb8b41260556273c9212ce0859ec3043322dadcc
alexmatica <alexbv2301@gmail.com>
2017-10-29 16:05:32 +0200

    Added Native app which sends an email

---
+++ app/src/main/java/mfie1944/ubb/ro/travellerapp/MainActivity.java
@@ -0,0 +1,35 @@
+package mfie1944.ubb.ro.travellerapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "mfie1944.ubb.ro.travellerapp.LOL";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void connectHandle(View view){
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"alexbv2301@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Content");
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex){
+            Toast.makeText(this, "There are no email clients installed!", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+}
